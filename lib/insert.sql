451e2f430bc993032aced455ad691a8672536a8e
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-09-13 21:39:18 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 3, 'M', 'White', 'Active', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 2, 'M', 'Grey', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 2, 'F', 'Green', 'Sweet', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 4, 'M', 'Grey', 'Lazy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 7, 'F', 'Blue', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 1, 'M', 'Black', 'Grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 3, 'F', 'Black', 'Active', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 4, 'M', 'Red', 'Lazy', 1);
